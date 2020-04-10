c6460053ff5db118cb674e57889c853a157104f6
Ivanich99 <45899932+Ivanich99@users.noreply.github.com>
2020-04-11 00:23:57 +0500

    Первая попытка

---
+++ FruitNinja/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap outJoin = Resource1.pear,
+            join = Resource1.hand;
+
+        private Point _targetPosition = new Point(300, 300);
+
+        private Point direction = Point.Empty;
+        private int _score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint, true);
+            UpdateStyles();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            timer2.Interval = r.Next(300, 1000);
+            direction.X = r.Next(-30, 30);
+            direction.Y = r.Next(-30, 30);
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            var lokalPosition = this.PointToClient(Cursor.Position);
+
+            _targetPosition.X += direction.X;
+            _targetPosition.Y += direction.Y;
+
+            if (_targetPosition.X < 10 || _targetPosition.X > 500)
+            {
+                direction.X *= -1;
+            }
+            if (_targetPosition.Y < 10 || _targetPosition.Y > 500)
+            {
+                direction.Y *= -1;
+            }
+            Point startPoint = new Point(lokalPosition.X - _targetPosition.X, lokalPosition.Y - _targetPosition.Y);
+            float distanse = (float) Math.Sqrt(startPoint.X * startPoint.X + startPoint.Y * startPoint.Y);
+            if(distanse<20)
+            {
+                AddScore(1);
+            }
+
+            var takeOutJoin = new Rectangle(_targetPosition.X - 50, _targetPosition.Y - 50, 100, 100);
+            var takeJoin = new Rectangle(lokalPosition.X - 50, lokalPosition.Y - 50, 100, 100);
+            g.DrawImage(join, takeJoin);
+
+            g.DrawImage(outJoin, takeOutJoin);
+        
+        }
+        private void AddScore(int score)
+        {
+            _score += score;
+            lable.Text = _score.ToString();
+        }
+    }
+}
---
+++ FruitNinja/WindowsFormsApp3/Form1.designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.lable = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // lable
+            // 
+            this.lable.AutoSize = true;
+            this.lable.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lable.Location = new System.Drawing.Point(712, 9);
+            this.lable.Name = "lable";
+            this.lable.Size = new System.Drawing.Size(22, 25);
+            this.lable.TabIndex = 0;
+            this.lable.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp3.Resource1.baground;
+            this.ClientSize = new System.Drawing.Size(815, 475);
+            this.Controls.Add(this.lable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label lable;
+    }
+}
+
